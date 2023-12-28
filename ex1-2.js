106bf4f5eae142efe463ec1b3be1d61946c60ba6
asahi1019 <131091854+asahi1019@users.noreply.github.com>
2023-12-28 22:01:53 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,1 +1,1 @@
-function nazo1(){if(document.getElementById('answer1').value=='フェルナンド・トーレス'){alert('正解！');}else{alert('不正解');}}
+function nazo1(){if(document.getElementById('answer1').value=='トーレス'){alert('正解！');}else{alert('不正解');}}
